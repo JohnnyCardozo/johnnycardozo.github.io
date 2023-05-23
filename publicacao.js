1ce2ce454a9cefb2b1d378019caa6be3240de2b1
João Cardozo <jopecardozo@gmail.com>
2023-05-23 19:08:45 -0300

    adicionado cor nos inputs de texto que deixa vermelho sempre que apertar espaço

--- publicacao.js
+++ publicacao.js
@@ -1,14 +1,27 @@
 const elementoComInput = document.querySelectorAll(".campo__input");
 
-function pegaID(indexElementoDoConjunto) {
-    let elemento = elementoComInput[indexElementoDoConjunto];
-    console.log(elemento.id);
-}
-
 for (let contador = 0; contador < elementoComInput.length; contador++) {
     const campos = elementoComInput[contador];
 
     campos.onclick = function () {
         pegaID(contador);
     }
+
+    campos.onkeydown = function (evento) {
+
+        if (evento.code === 'Space') {
+            campos.classList.add('apresentacao__destaque');
+        }
+    }
+
+    campos.onkeyup = function (evento) {
+        if (evento.code === 'Space') {
+            campos.classList.remove('apresentacao__destaque');
+        } 
+    }
+}
+
+function pegaID(indexElementoDoConjunto) {
+    const elemento = elementoComInput[indexElementoDoConjunto];
+    console.log(elemento.id);
 }
