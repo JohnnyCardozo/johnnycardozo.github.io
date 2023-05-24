edba4ead0bad85d2df17562ccf5c26479f9fc774
João Cardozo <jopecardozo@gmail.com>
2023-05-24 17:01:03 -0300

    alterações minimas do código

--- publicacao.js
+++ publicacao.js
@@ -3,12 +3,11 @@ const elementoComInput = document.querySelectorAll(".campo__input");
 for (let contador = 0; contador < elementoComInput.length; contador++) {
     const campos = elementoComInput[contador];
 
-    campos.onclick = function () {
+    campos.onclick = () => {
         pegaID(contador);
     }
 
     campos.onkeydown = function (evento) {
-
         if (evento.code === 'Space') {
             campos.classList.add('apresentacao__destaque');
         }
