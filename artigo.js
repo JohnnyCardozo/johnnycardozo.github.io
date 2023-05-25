4d98bfa5a338034bf68a45cbf55e9a6b6c4e4f00
João Cardozo <jopecardozo@gmail.com>
2023-05-25 18:27:06 -0300

    organizado nome de classes e ids na pagina de publicacao

--- artigo.js
+++ artigo.js
@@ -4,8 +4,8 @@ console.log(formularioArtigo);
 formularioArtigo.addEventListener("submit", (evento) => {
     evento.preventDefault()
 
-    const tituloArtigo = evento.target.elements['tituloPublicacao'].value;
-    const emailArtigo = evento.target.elements['emailPublicacao'].value;
+    const tituloArtigo = evento.target.elements['publicacaoTitulo'].value;
+    const emailArtigo = evento.target.elements['publicacaoEmail'].value;
 
     criarArtigo(tituloArtigo, emailArtigo);
 });
