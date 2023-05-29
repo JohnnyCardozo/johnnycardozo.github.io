2415d9bdfb699a9af05d1ab4c49c2aed05b95d53
João Cardozo <jopecardozo@gmail.com>
2023-05-29 14:26:02 -0300

    Adicionado o localStorage na página dde cadastro de jogos

--- Js/cadastroJogo.js
+++ Js/cadastroJogo.js
@@ -1,19 +1,41 @@
 const formularioCadastroJogo = document.getElementById("novoJogo");
-console.log(formularioCadastroJogo);
+const jogos = JSON.parse(localStorage.getItem("Jogos")) || []; 
+
+jogos.forEach((elemento) => {
+    cadastrarJogo(elemento)
+})
 
 formularioCadastroJogo.addEventListener("submit", (evento) => {
     evento.preventDefault()
 
-    const tituloCadastroJogo = evento.target.elements['cadastroNomeJogo'].value;
-    const desenvolvedoraCadastroJogo = evento.target.elements['cadastroDesenvolvedora'].value;
-    const publicadoraCadastroJogo = evento.target.elements['cadastroPublicadora'].value;
-    const dataLancamentoCadastroJogo = evento.target.elements['cadastroDataLancamento'].value;
-    const numeroJogadoresCadastro = evento.target.elements['cadastroNumeroJogadores'].value;
+    const tituloCadastroJogo = evento.target.elements['cadastroNomeJogo'];
+    const desenvolvedoraCadastroJogo = evento.target.elements['cadastroDesenvolvedora'];
+    const publicadoraCadastroJogo = evento.target.elements['cadastroPublicadora'];
+    const dataLancamentoCadastroJogo = evento.target.elements['cadastroDataLancamento'];
+    const numeroJogadoresCadastro = evento.target.elements['cadastroNumeroJogadores'];
 
-    cadastrarJogo(tituloCadastroJogo, desenvolvedoraCadastroJogo, publicadoraCadastroJogo, dataLancamentoCadastroJogo, numeroJogadoresCadastro);
+    const jogoAtual = {
+        "nome": tituloCadastroJogo.value,
+        "desenvolvedora": desenvolvedoraCadastroJogo.value,
+        "publicadora": publicadoraCadastroJogo.value,
+        "dataLançamento": dataLancamentoCadastroJogo.value,
+        "numeroJogadores": numeroJogadoresCadastro.value
+    }
+
+    cadastrarJogo(jogoAtual);
+
+    jogos.push(jogoAtual);
+
+    localStorage.setItem("Jogos", JSON.stringify(jogos));
+
+    tituloCadastroJogo.value = "";
+    desenvolvedoraCadastroJogo.value = "";
+    publicadoraCadastroJogo.value = "";
+    dataLancamentoCadastroJogo.value = "";
+    numeroJogadoresCadastro.value = "";
 });
 
-function cadastrarJogo(nome, desenvolvedora, publicadora, dataLancamento, numeroJogadores) {
+function cadastrarJogo(jogo) {
     const listaJogos = document.getElementById("listaJogos");
 
     const itemJogo = document.createElement('li');
@@ -25,27 +47,27 @@ function cadastrarJogo(nome, desenvolvedora, publicadora, dataLancamento, numero
 
     const nomeJogo = document.createElement('p');
     nomeJogo.classList.add("nome__jogo");
-    nomeJogo.innerHTML = 'Nome:  ' + nome
+    nomeJogo.innerHTML = 'Nome:  ' + jogo.nome
     novoInformacaoJogo.appendChild(nomeJogo);
 
     const desenvolvedoraJogo = document.createElement('p');
     desenvolvedoraJogo.classList.add("desenvolvedora__jogo");
-    desenvolvedoraJogo.innerHTML = 'Desenvolvedora:  ' + desenvolvedora;
+    desenvolvedoraJogo.innerHTML = 'Desenvolvedora:  ' + jogo.desenvolvedora;
     novoInformacaoJogo.appendChild(desenvolvedoraJogo);
 
     const publicadoraJogo = document.createElement('p');
     publicadoraJogo.classList.add("publicadora__jogo");
-    publicadoraJogo.innerHTML = 'Publicadora:  ' + publicadora;
+    publicadoraJogo.innerHTML = 'Publicadora:  ' + jogo.publicadora;
     novoInformacaoJogo.appendChild(publicadoraJogo);
 
     const dataJogo = document.createElement('p');
     dataJogo.classList.add("data__jogo");
-    dataJogo.innerHTML = 'Data de Lançamento:  ' + dataLancamento;
+    dataJogo.innerHTML = 'Data de Lançamento:  ' + jogo.dataLançamento;
     novoInformacaoJogo.appendChild(dataJogo);
 
     const numeroJogadoresJogo = document.createElement('p');
     numeroJogadoresJogo.classList.add("numero__jogadores__jogo");
-    numeroJogadoresJogo.innerHTML = 'Número de Jogadores:  ' + numeroJogadores;
+    numeroJogadoresJogo.innerHTML = 'Número de Jogadores:  ' + jogo.numeroJogadores;
     novoInformacaoJogo.appendChild(numeroJogadoresJogo);
 
     listaJogos.appendChild(itemJogo)
